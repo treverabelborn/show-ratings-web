ec2899b2650e5bd3f86a1bd62c4fc3b42608ac69
Trever Abelborn <trever@abelborn.com>
2024-01-23 17:41:04 -0600

    initial sorting

--- main.js
+++ main.js
@@ -3,16 +3,36 @@ const showsDataUrl = 'https://show-ratings-scraper-output.s3.us-east-2.amazonaws
 requirejs(['papa-parse'],
     function() {
         const papa = requirejs('papa-parse');
-        const showsDiv = document.getElementById('showsDiv')
+        const showsDiv = document.getElementById('shows');
+        const sortKey = document.getElementById('sortControls').value;
 
         fetch(showsDataUrl)
             .then(response => response.text())
             .then(text => papa.parse(text, { header: true }))
-            .then(json => insertContent(json.data, showsDiv));
+            .then(json => sortShows(json.data, sortKey))
+            .then(sorted => insertContent(sorted, showsDiv));
 });
 
+function sortShows(shows, sortKey) {
+    switch(sortKey) {
+        // Add second sorting order
+        case 'criticRating': return shows.sort((a, b) => Number(b.critic_rating) - Number(a.critic_rating));
+        case 'audienceRating': return shows.sort((a, b) => Number(b.audience_rating) - Number(a.audience_rating));
+        case 'avgRating': return shows.sort((a, b) => getAvgRating(b) - getAvgRating(a));
+        default: return shows;
+    }
+}
+
+function getAvgRating(show) {
+    const c = show.critic_rating;
+    const a = show.audience_rating;
+    if (!a && !c) return 0;
+    if (!c) return a;
+    if (!a) return c;
+    return a + c / 2;
+}
+
 function insertContent(shows, showsDiv) {
-    console.log(shows);
     shows.forEach(show => showsDiv.appendChild(getShowElement(show)))
 }
 
@@ -39,10 +59,10 @@ function getShowElement(show) {
 
     // ratings
     const criticRating = document.createElement('h3')
-    criticRating.innerHTML = (`Critic Rating: ${show.critic_rating}`)
+    criticRating.innerHTML = (`Critic Rating: ${show.critic_rating ? `${show.critic_rating}%` : '--'}`)
     criticRating.classList.add('text-2xl')
     const audienceRating = document.createElement('h3')
-    audienceRating.innerHTML = (`Audience Rating: ${show.audience_rating}`)
+    audienceRating.innerHTML = (`Audience Rating: ${show.audience_rating ? `${show.audience_rating}%` : '--'}`)
     audienceRating.classList.add('text-2xl')
     
     // image
